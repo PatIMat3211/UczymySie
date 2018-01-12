7a308baf0baf14f4f504ac711a18187327fa92b1
Tails <Tails@192.168.1.110>
2018-01-12 18:00:11 +0100

    heh

--- testy/src/me/testy/test/Main.java
+++ testy/src/me/testy/test/Main.java
@@ -3,7 +3,7 @@ package me.testy.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Heh!");
+		System.out.println("Im working on computer z windows XD!");
 	}
 
 }
